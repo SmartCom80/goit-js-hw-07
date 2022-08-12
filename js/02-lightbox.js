762ddd7882e6c843c27980fc2f6ce70278f473ca
SmartCom80 <smartcom.ter@gmail.com>
2022-08-12 09:40:21 +0300

    fixed bug

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -19,29 +19,15 @@ function makeGalleryMarkup(galleryItems) {
     .join(" ");
 }
 
-// 3. Получаем из HTML ссылку на коневой элемент Галереи
+// 2. Получаем из HTML ссылку на коневой элемент Галереи
 const galleryRef = document.querySelector(".gallery");
 
-// 2. Добавляем в код HTML, разметку для элементов галереи.
+// 4. Добавляем в код HTML, разметку для элементов галереи.
 galleryRef.insertAdjacentHTML("afterbegin", makeGalleryMarkup(galleryItems));
 
-// 3. Добавляем слушатель события и вызов конструктора модального окна
-galleryRef.addEventListener("click", (event) => {
-  event.preventDefault();
-  const evt = event.target;
-
-  if (!evt.classList.contains("gallery__image")) {
-    return;
-  } else {
-    return modalShowGalleryItems();
-  }
-});
-
 // 4. Функция-конструктор из библиотеки SimpleLightBox
-function modalShowGalleryItems() {
-  var lightbox = new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionPosition: "bottom",
-    captionDelay: 250,
-  });
-}
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
